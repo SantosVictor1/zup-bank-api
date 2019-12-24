e77ce639199b92768bc927d71962595248507859
Victor Santos <victor.udi10@hotmail.com>
2019-12-24 11:07:28 -0300

    Novos métodos de teste

--- src/test/kotlin/br/com/zup/bank/UserServiceTest.kt
+++ src/test/kotlin/br/com/zup/bank/UserServiceTest.kt
@@ -3,7 +3,9 @@ package br.com.zup.bank
 import br.com.zup.bank.model.User
 import br.com.zup.bank.repository.UserRepository
 import br.com.zup.bank.service.UserService
+import br.com.zup.bank.service.exception.BankException
 import org.junit.Test
+import org.junit.jupiter.api.fail
 import org.junit.runner.RunWith
 import org.mockito.InjectMocks
 import org.mockito.Mock
@@ -22,16 +24,60 @@ class UserServiceTest {
     @Mock
     private lateinit var userRepository: UserRepository
 
-    @Test
-    fun existsByCpfTest() {
-        val user = User(1, "Victor", "02160795607", "victor@gmail.com")
+//    @Test(expected = BankException::class)
+//    fun existsByCpfTest() {
+//        val user = User(1, "Victor", "02160795607", "victor@gmail.com")
+//
+//        Mockito.`when`(userRepository.existsByCpf(user.cpf)).thenReturn(true)
+//        userService.createUser(user)
+//
+//        Mockito.verify(userRepository, Mockito.times(1)).existsByCpf(user.cpf)
+//    }
 
-        Mockito.`when`(userRepository.save(Mockito.eq(user))).thenThrow(RuntimeException())
+//    @Test(expected = BankException::class)
+//    fun existsByEmailTest() {
+//        val user = User(1, "Victor", "02160795607", "teste@gmail.com")
+//
+//        Mockito.`when`(userRepository.existsByEmail(user.email)).thenReturn(true)
+//        userService.createUser(user)
+//
+//        Mockito.verify(userRepository, Mockito.times(1)).existsByEmail(user.email)
+//    }
+
+//    @Test(expected = BankException::class)
+//    fun isNameNullTest() {
+//        var user = User(1, null, "02160795607", "teste@gmail.com")
+//
+//        userService.createUser(user)
+//    }
+
+//    @Test(expected = BankException::class)
+//    fun isNameEmptyTest() {
+//        var user = User(1, "", "02160795607", "teste@gmail.com")
+//
+//        userService.createUser(user)
+//    }
+
+//    @Test(expected = BankException::class)
+//    fun lengthNameTest() {
+//        var user = User(1, "an", "02160795607", "teste@gmail.com")
+//
+//        userService.createUser(user)
+//    }
+
+    @Test(expected = BankException::class)
+    fun isCpfNullTest() {
+        var user = User(1, "Victor", null, "victor@gmail.com")
 
         userService.createUser(user)
-
-        Mockito.verify(userRepository, Mockito.times(1)).save(user)
     }
+
+//    @Test(expected = BankException::class)
+//    fun isCpfEmptyTest() {
+//        var user = User(1, "Victor", "", "victor@gmail.com")
+//
+//        userService.createUser(user)
+//    }
 
     @Test
     fun getAllTest() {
