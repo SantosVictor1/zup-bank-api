00e1835e1efaa862398431f101ddd9fe206ad4fd
Victor Santos <victor.udi10@hotmail.com>
2020-01-22 11:07:15 -0300

    Correção nos scripts sql de teste

--- src/test/resources/scripts/AccountSQL.sql
+++ src/test/resources/scripts/AccountSQL.sql
@@ -4,18 +4,18 @@ INSERT INTO account(
     balance,
     is_active,
     acc_limit,
-    user_user_id) VALUES (1, "6548732156", 1000.0, true, 1000.0, 1);
+    user_id) VALUES (1, "6548732156", 1000.0, true, 1000.0, 1);
 INSERT INTO account(
     account_id,
     number,
     balance,
     is_active,
     acc_limit,
-    user_user_id) VALUES (2, "6548732157", 1000.0, true, 1000.0, 2);
+    user_id) VALUES (2, "6548732157", 1000.0, true, 1000.0, 2);
 INSERT INTO account(
     account_id,
     number,
     balance,
     is_active,
     acc_limit,
-    user_user_id) VALUES (3,  "6548732158", 1000.0, true, 1000.0, 3);
+    user_id) VALUES (3,  "6548732158", 1000.0, true, 1000.0, 3);
--- src/test/resources/scripts/TransferSQL.sql
+++ src/test/resources/scripts/TransferSQL.sql
@@ -3,7 +3,7 @@ INSERT INTO transfer(
     date,
     transfer_status,
     value,
-    destiny_account_account_id,
-    origin_account_account_id,
+    destiny_account_id,
+    origin_account_id,
     error_code
     ) VALUES (1, "2020-01-16 10:25:00", "COMPLETED", 100.0, 1, 2, "");
