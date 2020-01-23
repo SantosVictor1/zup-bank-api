9a15ed065d5c6a5924062307e70842e9a91e7ee3
Victor Santos <victor.udi10@hotmail.com>
2020-01-23 15:18:32 -0300

    Formatação de código

--- src/test/kotlin/br/com/zup/bank/integrated/AccountControllerTest.kt
+++ src/test/kotlin/br/com/zup/bank/integrated/AccountControllerTest.kt
@@ -10,7 +10,6 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.http.MediaType
-import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.jdbc.Sql
 import org.springframework.test.context.jdbc.SqlGroup
 import org.springframework.test.context.junit4.SpringRunner
