ace062369306751d9b65b083f9d49745ca5c9a0c
Victor Santos <victor.udi10@hotmail.com>
2020-01-31 16:46:23 -0300

    Testes do camunda

--- src/main/kotlin/br/com/zup/bank/BankApplication.kt
+++ src/main/kotlin/br/com/zup/bank/BankApplication.kt
@@ -1,9 +1,11 @@
 package br.com.zup.bank
 
+import org.camunda.bpm.spring.boot.starter.annotation.EnableProcessApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
+@EnableProcessApplication
 class BankApplication
 
 fun main(args: Array<String>) {
